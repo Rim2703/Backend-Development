448b2546fc4935ab8243ef19046099a3534eda98
Rimsha <khanrimsha976@gmail.com>
2022-08-08 17:24:12 +0530

    Added Files

--- src/routes/route.js
+++ src/routes/route.js
@@ -17,14 +17,17 @@ router.get('/test-me', function (req, res) {
     thirdQue.lower("BACKEND DEVELOPMENT")
     thirdQue.upper("functionup plutonium batch")
     
+    // first Question
     let arr = ['january', 'february', 'march', 'april', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
     let a = lodash.chunk(arr, 4)
     console.log(a)
     
+    // Second Question
     let odd = [1, 3, 5, 7, 11, 13, 15, 17, 19, 21]
     let b = lodash.tail(odd)
     console.log(b)
     
+    // Third Question
     let arr1 = [1, 2, 4, 2, 1]
     let arr2 = [45, 7, 56, 0]
     let arr3 = [18, 45, 7, 11, 67]
@@ -33,6 +36,7 @@ router.get('/test-me', function (req, res) {
     let unique = lodash.union(arr1, arr2, arr3, arr4, arr5)
     console.log(unique)
 
+    // Fourth Question
     let comb = [['horror', 'The Shining'], ['drama', 'Titanic'], ['thriller', 'Shutter Island'], ['fantasy', 'Pans Labyrinth']]
     let objList = lodash.fromPairs(comb)
     console.log(objList)
