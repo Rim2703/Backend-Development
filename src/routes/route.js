e02bc6c54ef32a0a56f004b786da334d71341a0c
Rimsha <khanrimsha976@gmail.com>
2022-08-08 16:15:35 +0530

    new file

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const lodash = require('lodash')
 
 const abc = require('./intro')
 const firstQue = require('../logger/logger.js')
@@ -7,7 +8,7 @@ const thirdQue = require('../validator/formatter.js')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    console.log("My batch is",abc.name)
+    console.log("My batch is", abc.name)
     firstQue.welcome()
     secondQue.date()
     secondQue.month()
@@ -16,10 +17,29 @@ router.get('/test-me', function (req, res) {
     thirdQue.lower("BACKEND DEVELOPMENT")
     thirdQue.upper("functionup plutonium batch")
     
+    let arr = ['january', 'february', 'march', 'april', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+    let a = lodash.chunk(arr, 4)
+    console.log(a)
+    
+    let odd = [1, 3, 5, 7, 11, 13, 15, 17, 19, 21]
+    let b = lodash.tail(odd)
+    console.log(b)
+    
+    let arr1 = [1, 2, 4, 2, 1]
+    let arr2 = [45, 7, 56, 0]
+    let arr3 = [18, 45, 7, 11, 67]
+    let arr4 = [2, 5, 8, 0, 8]
+    let arr5 = [16, 90, 5, 8]
+    let unique = lodash.union(arr1, arr2, arr3, arr4, arr5)
+    console.log(unique)
+
+    let comb = [['horror', 'The Shining'], ['drama', 'Titanic'], ['thriller', 'Shutter Island'], ['fantasy', 'Pans Labyrinth']]
+    let objList = lodash.fromPairs(comb)
+    console.log(objList)
     res.send('My first ever api! I will Create it.')
 });
 
-router.get('/test-you',function (req,res){
+router.get('/test-you', function (req, res) {
     res.send("My Second route")
 });
 
