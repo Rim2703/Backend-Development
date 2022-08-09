3971166f7157f2d18dcac37e9cde133fd0cd0523
Rimsha <khanrimsha976@gmail.com>
2022-08-09 20:32:11 +0530

    New Api

--- src/index.js
+++ src/index.js
@@ -10,6 +10,39 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', route);
 
+app.get("/sol1", function (req, res) {
+    let arr= [1,2,3,5,6,7]
+    let total = 0;
+    for (let i in arr) {
+        total += arr[i];
+    }
+  
+    let lastDigit= arr.pop()
+    let consecutiveSum= lastDigit * (lastDigit+1) / 2
+    let missingNumber= consecutiveSum - total
+  
+    res.send(  { data: missingNumber  }  );
+  });
+  
+  app.get("/sol2", function (req, res) {
+    //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+    
+    let arr= [33, 34, 35, 37, 38]
+    let len= arr.length
+  
+    let total = 0;
+    for (let i in arr) {
+        total += arr[i];
+    }
+  
+    let firstDigit= arr[0]
+    let lastDigit= arr.pop()
+    let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+    let missingNumber= consecutiveSum - total
+   
+    res.send(  { data: missingNumber  }  );
+  });
+  
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
--- src/routes/route.js
+++ src/routes/route.js
@@ -34,6 +34,40 @@ router.get("/shoes", function(req, res){
     res.send("dummy response")
 })
 
+router.get("/sol1", function (req, res) {
+    let arr= [1,2,3,5,6,7]
+    let total = 0;
+    for (let i in arr) {
+        total += arr[i];
+    }
+  
+    let lastDigit= arr.pop()
+    let consecutiveSum= lastDigit * (lastDigit+1) / 2
+    let missingNumber= consecutiveSum - total
+  
+    res.send(  { data: missingNumber  }  );
+  });
+  
+  router.get("/sol2", function (req, res) {
+    //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+    
+    let arr= [33, 34, 35, 37, 38]
+    let len= arr.length
+  
+    let total = 0;
+    for (let i in arr) {
+        total += arr[i];
+    }
+  
+    let firstDigit= arr[0]
+    let lastDigit= arr.pop()
+    let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+    let missingNumber= consecutiveSum - total
+   
+    res.send(  { data: missingNumber  }  );
+  });
+  
+
 // uses query params
 router.get('/candidates', function(req, res){
     console.log('Query paramters for this request are '+JSON.stringify(req.query))
@@ -98,7 +132,6 @@ router.get("/films/:filmId", function(req, res){
                return res.send(film)
            }
        }
-
        //if there is no match give an error response
        res.send("The film id doesn't match any movie")
 })
