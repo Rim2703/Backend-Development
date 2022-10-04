aeec4153d59e37a1b85f641894634fb0b81f65c0
Rimsha <khanrimsha976@gmail.com>
2022-10-04 15:24:48 +0530

    Caching Redis

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -5,18 +5,17 @@ const { promisify } = require("util");
 
 //Connect to redis
 const redisClient = redis.createClient(
-  13190,
-  "redis-13190.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+  10976,
+  "redis-10976.c301.ap-south-1-1.ec2.cloud.redislabs.com",
   { no_ready_check: true }
 );
-redisClient.auth("gkiOIPkytPI3ADi14jHMSWkZEo2J5TDG", function (err) {
+redisClient.auth("63Fkn6vvGYtfftugyWgr7LrK8Emyqedh", function (err) {
   if (err) throw err;
 });
 
 redisClient.on("connect", async function () {
   console.log("Connected to Redis..");
 });
-
 
 
 //1. connect to the server
@@ -35,9 +34,9 @@ const createAuthor = async function (req, res) {
 };
 
 const fetchAuthorProfile = async function (req, res) {
-  let cahcedProfileData = await GET_ASYNC(`${req.params.authorId}`)
-  if(cahcedProfileData) {
-    res.send(cahcedProfileData)
+  let cachedProfileData = await GET_ASYNC(`${req.params.authorId}`)
+  if(cachedProfileData) {
+    res.send(JSON.parse(cachedProfileData))
   } else {
     let profile = await authorModel.findById(req.params.authorId);
     await SET_ASYNC(`${req.params.authorId}`, JSON.stringify(profile))
